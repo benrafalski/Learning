2af4c9df72a23da34ddc55d52cca87aa9ca9f4ff
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-11 19:00:17 -0700

    init

---
+++ simple_storage.sol
@@ -0,0 +1,46 @@
+// version 
+pragma solidity >=0.6.0 <0.9.0; 
+
+contract SimpleStroage {
+
+    // types
+    uint256 public favoriteNumber;
+    // bool favotiteBool = true;
+    // string favString = "string"; 
+    // int256 = favInt;
+    // address favAddress = "0xb6e2549f020Be31f56C2Eac1E8787b514Fb53Dd6";
+    // bytes32 favBytes = "cat";
+
+
+    // struct
+    struct People {
+        uint256 favNumber;
+        string name;
+    }
+
+    People public person = People({
+        favNumber: 2, 
+        name: "Ben"
+    });
+
+    People[] public people;
+
+
+
+    // functions
+    function store(uint256 _favNum) public{
+        favoriteNumber = _favNum;
+    }
+
+    function addPerson(string memory _name, uint256 _favNum) public{
+        people.push(People({
+            favNumber: _favNum,
+            name: _name
+        }));
+    }
+
+
+
+
+
+}
