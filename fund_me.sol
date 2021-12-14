f83139a6960eedb5aeb6932b7b86e4e7aed10d83
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-14 15:25:04 -0700

    lesson 3 done

--- fund_me.sol
+++ fund_me.sol
@@ -1,18 +1,58 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.9.0;
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract fundMe{
+    // using keyword
+    // using SafeMathChainLink for uint256;
 
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address owner;
+
+    // constuctor
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    
 
 
     // function used to pay for things
    function fund() public payable{
        // msg. sender and value are keywords
+       uint256 minUSD = 50 * 10 ** 18; // 50 x 1^18
+       // require statement
+       require(getConversionRate(msg.value) >= minUSD, "Not enough eth");
        addressToAmountFunded[msg.sender] += msg.value;
        // what is the eth to usd conversion rate?
        // use an oracle
+       funders.push(msg.sender);
    } 
+
+   function getConversionRate(uint256 ethAmt) public view returns(uint256) {
+       uint256 ethPrice; /*  = getPrice(); */
+       uint256 ethAmtInUsd = (ethPrice * ethAmt) / 100000000;
+       return ethAmtInUsd;
+   }
+
+    // modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+   function withdraw() payable onlyOwner public {
+       
+    //    msg.sender.transfer(address(this).balance);
+    // for loops 
+    for(uint256 i=0; i < funders.length; i++){
+        address funder = funders[i];
+        addressToAmountFunded[funder] = 0;
+    }
+    funders = new address[](0);
+    
+   }
 }
