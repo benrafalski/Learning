a4faa0cdb86ea679d9b431bfa066494c2e102b13
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-20 11:48:12 -0700

    .env and deploying

--- brownie_ss/scripts/deploy.py
+++ brownie_ss/scripts/deploy.py
@@ -1,11 +1,12 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, network, config
 
-
+import os
 
 def deploy_simple_storage():
     # using accounts lib
-    account = accounts[0]
+    # account = accounts[0]
     # account = accounts.load('test-account')
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
     print(stored_value)
@@ -14,5 +15,12 @@ def deploy_simple_storage():
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
 
+def get_account():
+    if (network.show_active() == "development"):
+        return accounts[0]
+    else:
+        return accounts.load('test-account')
+
 def main():
+    print(os.getenv("WEB3_INFURA_PROJECT_ID"))
     deploy_simple_storage()
