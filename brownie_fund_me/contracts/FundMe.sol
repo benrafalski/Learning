8716fe61311e30bc4818166d6be0eea46858c642
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-21 13:59:55 -0700

    contract deployed

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.9.0;
+
+
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
@@ -30,12 +32,12 @@ contract FundMe{
    } 
 
    function getVersion() public view returns (uint256) {
-       AggregatorV3Interface priceFeed = AggregatorV3Interface();
+       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
        return priceFeed.version();
    }
 
    function getPrice() public view returns (uint256) {
-       AggregatorV3Interface priceFeed = AggregatorV3Interface();
+       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
        (,int256 answer,,,) = priceFeed.latestRoundData();
        return uint256(answer * 10000000000);
    }
