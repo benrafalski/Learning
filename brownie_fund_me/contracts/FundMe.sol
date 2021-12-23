dcf072292c09b2518d8a43e4a50cf6c4c66dee15
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-22 18:53:34 -0700

    contract updated

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -13,9 +13,11 @@ contract FundMe{
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
     address owner;
+    AggregatorV3Interface public priceFeed;
 
     // constuctor
-    constructor() public {
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
 
@@ -32,12 +34,12 @@ contract FundMe{
    } 
 
    function getVersion() public view returns (uint256) {
-       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    //    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
        return priceFeed.version();
    }
 
    function getPrice() public view returns (uint256) {
-       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    //    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
        (,int256 answer,,,) = priceFeed.latestRoundData();
        return uint256(answer * 10000000000);
    }
