06f13d3e1a4f3a28d39ca0add183136361b70117
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-24 11:51:12 -0700

    deploy script cleanup

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,5 +1,7 @@
-from brownie import FundMe, network, config
+from brownie import FundMe, network, config, MockV3Aggregator
+from brownie_fund_me.scripts.helper_functions import deploy_mocks
 from scripts.helper_functions import get_account
+from web3 import Web3
 
 def deploy_fund_me():
     account = get_account()
@@ -7,10 +9,14 @@ def deploy_fund_me():
     # else, deploy mocks
     if network.show_active() != "development":
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address # uses the most recently deployed mock aggregator
+
     fund_me = FundMe.deploy(
-        ,
+        price_feed_address,
         {"from": account}, 
-        publish_source=True
+        publish_source=config["networks"][network.show_active()].get("verify")
     )
     print(f"Contract deployed to {fund_me.address}")
 
