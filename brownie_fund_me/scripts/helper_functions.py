f267c735e64056e31dc59acb8e10cb6a20636b6c
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-12-31 20:34:27 -0700

    test if statement error fixed

--- brownie_fund_me/scripts/helper_functions.py
+++ brownie_fund_me/scripts/helper_functions.py
@@ -2,6 +2,7 @@ from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIORNMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIORNMENTS = ["mainnet-fork"]
 
 
 DECIMALS = 8
@@ -9,10 +10,10 @@ STARTING_PRICE = 200000000
 
 
 def get_account():
-    if (network.show_active() in LOCAL_BLOCKCHAIN_ENVIORNMENTS):
+    if (network.show_active() in LOCAL_BLOCKCHAIN_ENVIORNMENTS or network.show_active() in FORKED_LOCAL_ENVIORNMENTS):
         return accounts[0]
     else:
-        return accounts.load('test-account')
+        return accounts.load('test-wallet')
 
 
 def deploy_mocks():
@@ -24,3 +25,4 @@ def deploy_mocks():
         mock_aggregator = MockV3Aggregator.deploy(DECIMALS ,Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()})
     
     print("Mocks deployed...")
+    
--- brownie_fund_me/tests/test_fund_me.py
+++ brownie_fund_me/tests/test_fund_me.py
@@ -1,6 +1,6 @@
 from scripts.helper_functions import LOCAL_BLOCKCHAIN_ENVIORNMENTS, get_account
 from scripts.deploy import deploy_fund_me
-from brownie import network, accounts
+from brownie import network, accounts, exceptions
 import pytest
 
 def test_can_fund_and_withdraw():
@@ -16,11 +16,13 @@ def test_can_fund_and_withdraw():
     assert fund_me.addressToAmountFunded(account.address) == 0
 
 def test_only_owner_can_withdraw():
-    if network.show_active not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
         pytest.skip("only for local testing")
-        # account = get_account()
-        fund_me = deploy_fund_me()
-        bad_actor = accounts.add()
-        # someone else tries to withdraw
+    # account = get_account()
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    # someone else tries to withdraw
+    # fund_me.withdraw({"from": bad_actor})
+    with pytest.raises(exceptions.VirtualMachineError):
         fund_me.withdraw({"from": bad_actor})
 
