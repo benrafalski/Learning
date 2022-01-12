2ad2a549b8a0b585fdff9a8cbcce9e6e2175a748
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-11 18:57:32 -0700

    close to deploying

--- smart-contract-lottery/scripts/deploy_lottery.py
+++ smart-contract-lottery/scripts/deploy_lottery.py
@@ -1,8 +1,12 @@
 from scripts.helpful_scripts import get_account
+from brownie import Lottery
 
 
 def deploy_lottery():
-    account = get_account()
+    account = get_account(id='test-wallet')
+    lottery = Lottery.deploy(
+        get_contract()
+    )
 
 def main():
     deploy_lottery()
--- smart-contract-lottery/scripts/helpful_scripts.py
+++ smart-contract-lottery/scripts/helpful_scripts.py
@@ -1,4 +1,5 @@
 from brownie import network, config, accounts, MockV3Aggregator
+from brownie_fund_me.scripts.helper_functions import deploy_mocks
 from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIORNMENTS = ["development", "ganache-local"]
@@ -9,11 +10,28 @@ DECIMALS = 8
 STARTING_PRICE = 200000000
 
 
-def get_account():
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    
     if (network.show_active() in LOCAL_BLOCKCHAIN_ENVIORNMENTS or network.show_active() in FORKED_LOCAL_ENVIORNMENTS):
         return accounts[0]
-    else:
-        return accounts.load('test-wallet')
+    # else:
+    #     return accounts.load('test-wallet')
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator
+}
+
+def get_contract(contract_name, ):
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+
+
 
 
 def deploy_mocks():
