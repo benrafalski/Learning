92d2f6c53bd69ccfd4a351c0ef9670955acd5e4c
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-16 15:09:07 -0700

    fixed getEntranceFee()

--- smart-contract-lottery/contracts/Lottery.sol
+++ smart-contract-lottery/contracts/Lottery.sol
@@ -40,11 +40,10 @@ contract Lottery is Ownable, VRFConsumerBase{
 
     function getEntranceFee() public view returns (uint256){
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
-        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
-        // $50, $2,000 / ETH
-        // 50/2,000
-        // 50 * 100000 / 2000
-        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        //return uint256(price);
+        uint256 adjustedPrice = uint256(price);  // 18 decimals
+        //return adjustedPrice;
+        uint256 costToEnter = (usdEntryFee * 10**23) / adjustedPrice;
         return costToEnter;
        
     }
--- smart-contract-lottery/tests/test_lottery_unit.py
+++ smart-contract-lottery/tests/test_lottery_unit.py
@@ -4,6 +4,6 @@ from web3 import Web3
 
 def test_get_entrance_fee():
     lottery = deploy_lottery()
-    excepted_entrance_fee = Web3.toWei(0.025,"ether")
+    excepted_entrance_fee = Web3.toWei(0.025, "ether")
     entrance_fee = lottery.getEntranceFee()
     assert excepted_entrance_fee == entrance_fee
