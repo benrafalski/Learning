4df4d85c7d3ba3b6271b58cd88c730bc396d072b
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-17 19:45:14 -0700

    fixed unit test for getentrancefee and configured .env

--- smart-contract-lottery/tests/test_lottery_unit.py
+++ smart-contract-lottery/tests/test_lottery_unit.py
@@ -1,8 +1,12 @@
 from brownie import Lottery, accounts, config, network
 from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIORNMENTS
 from web3 import Web3
+import pytest
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+        pytest.skip()
     lottery = deploy_lottery()
     excepted_entrance_fee = Web3.toWei(0.025, "ether")
     entrance_fee = lottery.getEntranceFee()
