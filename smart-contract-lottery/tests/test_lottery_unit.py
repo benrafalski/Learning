d764d9420f9b64468ea360ab13877c73210b6b01
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-19 15:27:55 -0700

    endLottery() test done

--- smart-contract-lottery/tests/test_lottery_unit.py
+++ smart-contract-lottery/tests/test_lottery_unit.py
@@ -1,6 +1,6 @@
 from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIORNMENTS, get_account
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIORNMENTS, get_account, fund_with_link
 from web3 import Web3
 import pytest
 
@@ -27,3 +27,26 @@ def test_can_start_and_enter_lottery():
     lottery.startLottery({"from": get_account(id='test-wallet')})
     lottery.enter({"from": account, "value": lottery.getEntranceFee()})
     assert lottery.players(0) == account
+
+def test_can_end_lottery():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+        pytest.skip()
+    lottery = deploy_lottery();
+    account = get_account(id='test-wallet')
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    assert lottery.lottery_state() == 2
+
+def test_can_pick_winner_correctly():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+        pytest.skip()
+    lottery = deploy_lottery();
+    account = get_account(id='test-wallet')
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": get_account(index=0), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    
