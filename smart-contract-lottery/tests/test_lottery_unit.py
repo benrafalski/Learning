ad3d7f0176cef2820721217f4f10b10a2143b807
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-18 18:51:34 -0700

    test if owner can enter lottery

--- smart-contract-lottery/tests/test_lottery_unit.py
+++ smart-contract-lottery/tests/test_lottery_unit.py
@@ -1,6 +1,6 @@
-from brownie import Lottery, accounts, config, network
+from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIORNMENTS
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIORNMENTS, get_account
 from web3 import Web3
 import pytest
 
@@ -11,3 +11,10 @@ def test_get_entrance_fee():
     excepted_entrance_fee = Web3.toWei(0.025, "ether")
     entrance_fee = lottery.getEntranceFee()
     assert excepted_entrance_fee == entrance_fee
+
+def test_cannot_enter_unless_started():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIORNMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
