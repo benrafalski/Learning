a5f4c958305bcd578b379d262e79bf251ecd52d4
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-01-28 18:33:18 -0700

    get_weth working

--- aave_brownie/scripts/get_weth.py
+++ aave_brownie/scripts/get_weth.py
@@ -1,5 +1,15 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, network, config
+
+
+
 def main():
     get_weth()
 
 def get_weth():
-    
+    account = get_account(id='test-wallet')
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": .01 * 10 ** 18})
+    print(f"recieved 0.01 weth")
+    return tx
+
