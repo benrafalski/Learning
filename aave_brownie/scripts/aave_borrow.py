48c3fe8d6858d11630e93835dd0886da5b0ef8e3
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-02-05 19:32:26 -0700

    lending needs an address

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -2,7 +2,7 @@
 
 
 from scripts.helpful_scripts import get_account
-from brownie import config, network
+from brownie import config, network, interface
 from scripts.get_weth import get_weth
 
 def main():
@@ -13,3 +13,8 @@ def main():
     lending_pool = get_lending_pool()
 
 def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    
