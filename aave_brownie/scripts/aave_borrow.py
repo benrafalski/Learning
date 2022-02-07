4b2f8285cb69c2c81c450e241faaabf653d0d29e
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-02-07 15:51:19 -0700

    lending works good

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -11,10 +11,12 @@ def main():
     if network.show_active() in ["mainnet_fork"]:
         get_weth()
     lending_pool = get_lending_pool()
+    print(lending_pool)
 
 def get_lending_pool():
-    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+    lending_pool_addresses_provider = interface.ILendingPoolAddressProvider(
         config["networks"][network.show_active()]["lending_pool_addresses_provider"]
     )
     lending_pool_address = lending_pool_addresses_provider.getLendingPool()
-    
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
