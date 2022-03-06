24d67dd17c97c67ab5ca8f8adf51a7eb0c54c003
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-03-06 16:00:17 -0700

    started mint funtion

--- NFTs/contracts/SimpleCollectible.sol
+++ NFTs/contracts/SimpleCollectible.sol
@@ -5,3 +5,13 @@ pragma solidity 0.6.6;
 
 import "@openzepplin/contracts/token/ERC721/ERC721.sol*";
 
+contract SimplieCollectible is ERC721 {
+    constructor () public ERC721 ("Dogie", "DOG"){
+        
+    }
+
+    function createCollectible() public returns (uint256) {
+        
+    }
+}
+
