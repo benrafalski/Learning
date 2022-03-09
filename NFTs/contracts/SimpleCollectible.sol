3edacfa4974007c703a5f6eb5c1accbf47cbbfaa
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-03-09 07:15:06 -0700

    createCollectible funtion done

--- NFTs/contracts/SimpleCollectible.sol
+++ NFTs/contracts/SimpleCollectible.sol
@@ -6,12 +6,16 @@ pragma solidity 0.6.6;
 import "@openzepplin/contracts/token/ERC721/ERC721.sol*";
 
 contract SimplieCollectible is ERC721 {
+    uint256 public tokenCounter;
     constructor () public ERC721 ("Dogie", "DOG"){
-        
+        tokenCounter = 0;
     }
 
     function createCollectible() public returns (uint256) {
-        
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
     }
 }
 
